80848afa2d4a7131a5802e55f427c18dc4b7dcae
Alex123-art-max <153431043+Alex123-art-max@users.noreply.github.com>
2023-12-26 11:01:18 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-const h1 = document.querySelector("h1")
+const audio = new Audio('./assets/audio.mp3')
 
 const size = 30
 
 const snake = [
     { x:270, y:240},
+    { x:300, y:240},
+    { x:330, y:240},
+    { x:360, y:240},
+    { x:390, y:240},
+    { x:420, y:240}
 ]
-
 const randomNumber = (min, max) => {
     return Math.round(Math.random() *(max - min) + min)
 }
@@ -24,8 +28,6 @@ const randomColor = () =>{
 
     return `rgb(${red}, ${green}, ${blue}`
 }
-
-h1.innerText = randomNumber(5,10)
 
 const food ={
     x:randomPosition(),
@@ -97,6 +99,45 @@ const drawGrid = () =>{
     ctx.stroke()
     }
 }
+const checkEat = () =>{
+    const head = snake[snake.length - 1]
+
+    if(head.x == food.x && head.y == food.y){
+    snake.push(head)
+    audio.play()
+
+    let x = randomPosition()
+    let y = randomPosition()
+
+    while(snake.find((position) => position.x == x && position.y == y)){
+        x = randomPosition()
+        y = randomPosition()
+    }
+    food.x = x
+    food.y = y
+    food.color = randomColor()
+    }
+}
+
+const checkCollision = () =>{
+    const head = snake[snake.length -1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length -2
+
+    const wallCollision = head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex.x == head.x && position.y == head.y
+    })
+
+    if (wallCollision){
+        alert("você perdeu!")
+    }
+}
+
+const gameOver = () =>{
+    direction = undefined
+}
 
 const GameLoop = () =>{
     clearInterval(loopId)
@@ -105,10 +146,12 @@ const GameLoop = () =>{
     drawFood()
     drawSnake()
     moveSnake()
+    checkEat()
+    checkCollision()
 
     loopId = setTimeout(() =>{
         GameLoop()
-    },300)
+    },200)
 }
 
 GameLoop()
